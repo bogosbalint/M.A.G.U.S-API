1786f6ed94b15565a3f3f4f299cf3315a93ef902
balint <bogosbalint2001@gmail.com>
2024-06-23 16:28:22 +0200

    Set up DbContext

---
+++ Api/Api/Models/Context.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Api.Models
+{
+    public class Context : DbContext
+    {
+
+        public Context(DbContextOptions options) : base(options) 
+        { 
+        }
+    }
+}
