d2e93afff04cd9f54817c57cf97502dc8c948381
balint <bogosbalint2001@gmail.com>
2024-06-25 23:35:10 +0200

    Removed Translation Model from DbContext

--- Api/Api/Models/Context.cs
+++ Api/Api/Models/Context.cs
@@ -5,7 +5,6 @@ namespace Api.Models
 {
     public class Context : DbContext
     {
-        public DbSet<Translation> Translations { get; set; }
         public DbSet<Category> Categories { get; set; }
         public DbSet<SubCategory> SubCategories { get; set; }
         public DbSet<Weapon> Weapons { get; set; }
