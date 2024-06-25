042946ca070940268d4dca2fc295d10170eb401c
balint <bogosbalint2001@gmail.com>
2024-06-25 23:23:40 +0200

    Created SubCategory Model

---
+++ Api/Api/Models/Weapon/SubCategory.cs
@@ -0,0 +1,9 @@
+﻿namespace Api.Models.Weapon
+{
+    public class SubCategory
+    {
+        public Guid Id { get; set; } = new Guid();
+        public List<Translation> Name { get; set; }
+        public List<Translation> Description { get; set; }
+    }
+}
