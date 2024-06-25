e679372ddcfb116de0154e1b91688c3cf7eb7555
balint <bogosbalint2001@gmail.com>
2024-06-25 23:07:05 +0200

    Created Translation Model

---
+++ Api/Api/Models/Translation.cs
@@ -0,0 +1,11 @@
+﻿namespace Api.Models
+{
+    public class Translation
+    {
+        public Guid Id { get; set; } = new Guid();
+        public string LangCode { get; set; }
+        public string Text { get; set; }
+        public string EntityType { get; set; }
+        public string FieldType { get; set; }
+    }
+}
