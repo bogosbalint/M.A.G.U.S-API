361362936bf05e5bce6513a582cb44b95a6613b5
balint <bogosbalint2001@gmail.com>
2024-06-25 23:31:01 +0200

    Added models to DbContext

--- Api/Api/Models/Context.cs
+++ Api/Api/Models/Context.cs
@@ -1,10 +1,14 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Api.Models.WeaponModels;
+using Microsoft.EntityFrameworkCore;
 
 namespace Api.Models
 {
     public class Context : DbContext
     {
-
+        public DbSet<Translation> Translations { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<SubCategory> SubCategories { get; set; }
+        public DbSet<Weapon> Weapons { get; set; }
         public Context(DbContextOptions options) : base(options) 
         { 
         }
