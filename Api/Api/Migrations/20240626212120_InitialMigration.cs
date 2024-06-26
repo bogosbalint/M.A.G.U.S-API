8deaae6ee20e6be763bbdd1b7e2a1c2c0ad14a4a
balint <bogosbalint2001@gmail.com>
2024-06-26 23:26:58 +0200

    Generated Migration

---
+++ Api/Api/Migrations/20240626212120_InitialMigration.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    NameHu = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    NameEn = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SubCategories",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    NameHu = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    NameEn = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DescriptionHu = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DescriptionEn = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SubCategories", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Weapons",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    NameHu = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    NameEn = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DescHu = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DescEn = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CategoryId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    SubCategoryId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Time = table.Column<int>(type: "int", nullable: false),
+                    NoOfDice = table.Column<int>(type: "int", nullable: false),
+                    Dice = table.Column<int>(type: "int", nullable: false),
+                    DamageModifier = table.Column<int>(type: "int", nullable: false),
+                    InitiatorValue = table.Column<int>(type: "int", nullable: false),
+                    AttackValue = table.Column<int>(type: "int", nullable: false),
+                    DefenseValue = table.Column<int>(type: "int", nullable: false),
+                    Weight = table.Column<double>(type: "float", nullable: false),
+                    PriceGold = table.Column<int>(type: "int", nullable: false),
+                    PriceSilver = table.Column<int>(type: "int", nullable: false),
+                    PriceCopper = table.Column<int>(type: "int", nullable: false),
+                    Disarm = table.Column<bool>(type: "bit", nullable: false),
+                    WeaponBreakage = table.Column<bool>(type: "bit", nullable: false),
+                    PunchTrough = table.Column<bool>(type: "bit", nullable: false),
+                    STP = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Weapons", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Weapons_Categories_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "Categories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Weapons_SubCategories_SubCategoryId",
+                        column: x => x.SubCategoryId,
+                        principalTable: "SubCategories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Weapons_CategoryId",
+                table: "Weapons",
+                column: "CategoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Weapons_SubCategoryId",
+                table: "Weapons",
+                column: "SubCategoryId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Weapons");
+
+            migrationBuilder.DropTable(
+                name: "Categories");
+
+            migrationBuilder.DropTable(
+                name: "SubCategories");
+        }
+    }
+}
